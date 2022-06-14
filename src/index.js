dd0dc696a0e1aa3fb43434395e3f595634d63576
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-14 17:47:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import App from "./App"
 
 ReactDOM.render(<App/>, document.getElementById("root"))
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
