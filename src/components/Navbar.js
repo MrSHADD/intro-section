4539f9d2d6a69b86d1960e16f67efa4bec6f2092
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-14 17:48:03 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,58 @@
 import React from "react";
 
 
+
 export default function Navbar(){
     return(
-        <div className="main">
-            <div className="nav-mobile">
-              <img src="./images/logo.svg" className="img--logo"/>
-              <img src="./images/icon-menu.svg" className="img--hamburger"/>
-            </div>
         
-        <div className="navbar">
-        <img src="./images/logo.svg" className="img--logo"/>
-         <div class="dropdown">
-           <h3 className="features">Features<img src="./images/icon-arrow-down.svg" className="arrow-down"/></h3>
-           <div class="dropdown-content">
-                <a href="#"><img src="./images/icon-todo.svg" className="img-db-icon"/>Todo List</a>
-                <a href="#"><img src="./images/icon-calendar.svg" className="img-db-icon"/>Calender</a>
-                <a href="#"><img src="./images/icon-reminders.svg" className="img-db-icon"/>Reminders</a>
-                <a href="#"><img src="./images/icon-planning.svg" className="img-db-icon"/>Planning</a>
-
+        <nav class="navbar">
+            <a href="#"><img src="./images/logo.svg" className="img--logo"/></a>
+            <div className="nav-menu">
+            <ul className="nav-menu-left">
+            <div class="dropdown">
+                <li className="nav-item">
+                    <a href="#" class="nav-link">Features<img src="./images/icon-arrow-down.svg" className="arrow-down" alt="arrowdown"/></a>
+                    <div class="dropdown-content">
+                        <a href="#"><img src="./images/icon-todo.svg" className="img-db-icon" alt="todo"/>Todo List</a>
+                        <a href="#"><img src="./images/icon-calendar.svg" className="img-db-icon" alt="Calender"/>Calender</a>
+                        <a href="#"><img src="./images/icon-reminders.svg" className="img-db-icon" alt="Reminder"/>Reminders</a>
+                        <a href="#"><img src="./images/icon-planning.svg" className="img-db-icon" alt="Planning"/>Planning</a>
+                    </div>
+                </li>
             </div>
-         </div>
-
-         <div className="dropdown-com">
-            <h3 className="company">Company<img src="./images/icon-arrow-down.svg" className="arrow-down"/></h3>
-            <div className="dropdown-content-com">
-                <a href="#">History</a>
-                <a href="#">Our Team</a>
-                <a href="#">Blog</a>
+            <div className="dropdown-com">
+                <li className="nav-item">
+                    <a href="#" class="nav-link">Company<img src="./images/icon-arrow-down.svg" className="arrow-down" alt="arrowdown"/></a>
+                    <div className="dropdown-content-com">
+                        <a href="#">History</a>
+                        <a href="#">Our Team</a>
+                        <a href="#">Blog</a>
+                    </div>
+            </li>
             </div>
-         </div>
-         
-            <h3 className="carrers">Carrers</h3>
-            <h3 className="about">About</h3>
-            <h3 className="login">Login</h3>
-            
-                <button className="btn--reg">Register</button>
-            {/* <div class="hamburger">
-                <img src="./images/icon-menu.svg"/>
-            </div> */}
+                <li className="nav-item">
+                    <a href="#" class="nav-link">Carrers</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+            </ul>
+            <ul className="nav-menu-right">
+                <li className="nav-item">
+                    <a href="#" class="nav-link">Login</a>
+                </li>
+                <li className="nav-item">
+                    <button className="btn--reg">Register</button>
+                </li>
+            </ul >
+            </div>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
         
-        </div>
-        </div>
     )
+     
 }
