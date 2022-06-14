42df7b2991e82ed9564e324505698dfee54b43db
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-14 18:11:10 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,47 +9,47 @@ export default function Navbar(){
             <a href="#"><img src="./images/logo.svg" className="img--logo" alt="logo"/></a>
             <div className="nav-menu">
             <ul className="nav-menu-left">
-            <niv class="dropdown">
+            <drop className="dropdown">
                 <li className="nav-item">
-                    <a href="#" class="nav-link">Features<img src="./images/icon-arrow-down.svg" className="arrow-down" alt="arrowdown"/></a>
-                    <div class="dropdown-content">
+                    <a href="#" className="nav-link">Features<img src="./images/icon-arrow-down.svg" className="arrow-down" alt="arrowdown"/></a>
+                    <div className="dropdown-content">
                         <a href="#"><img src="./images/icon-todo.svg" className="img-db-icon" alt="todo"/>Todo List</a>
                         <a href="#"><img src="./images/icon-calendar.svg" className="img-db-icon" alt="Calender"/>Calender</a>
                         <a href="#"><img src="./images/icon-reminders.svg" className="img-db-icon" alt="Reminder"/>Reminders</a>
                         <a href="#"><img src="./images/icon-planning.svg" className="img-db-icon" alt="Planning"/>Planning</a>
                     </div>
                 </li>
-            </niv>
-            <niv className="dropdown-com">
+            </drop>
+            <drop className="dropdown-com">
                 <li className="nav-item">
-                    <a href="#" class="nav-link">Company<img src="./images/icon-arrow-down.svg" className="arrow-down" alt="arrowdown"/></a>
+                    <a href="#" className="nav-link">Company<img src="./images/icon-arrow-down.svg" className="arrow-down" alt="arrowdown"/></a>
                     <div className="dropdown-content-com">
                         <a href="#">History</a>
                         <a href="#">Our Team</a>
                         <a href="#">Blog</a>
                     </div>
             </li>
-            </niv>
+            </drop>
                 <li className="nav-item">
-                    <a href="#" class="nav-link">Carrers</a>
+                    <a href="#" className="nav-link">Carrers</a>
                 </li>
                 <li className="nav-item">
-                    <a href="#" class="nav-link">About</a>
+                    <a href="#" className="nav-link">About</a>
                 </li>
             </ul>
             <ul className="nav-menu-right">
                 <li className="nav-item">
-                    <a href="#" class="nav-link">Login</a>
+                    <a href="#" className="nav-link">Login</a>
                 </li>
                 <li className="nav-item">
                     <button className="btn--reg">Register</button>
                 </li>
             </ul >
             </div>
-            <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
+            <div className="hamburger">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
             </div>
         </nav>
         
